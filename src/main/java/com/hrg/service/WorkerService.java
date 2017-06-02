0a7d7886c9fd13dddfc81c44b8400d0ed3f3a8e2
wanganui <827052997@qq.com>
2017-06-02 16:08:52 +0800

    service

--- src/main/java/com/hrg/service/WorkerService.java
+++ src/main/java/com/hrg/service/WorkerService.java
@@ -1,6 +1,8 @@
 package com.hrg.service;
 
 import com.hrg.model.Worker;
+import com.hrg.model.WorkerCriteria;
+import com.hrg.util.PageUtil;
 
 /**
  * Created by 82705 on 2017/6/1.
@@ -22,5 +24,14 @@ public interface WorkerService {
      * @throws Exception
      */
     String getWorkerPassWord(String account) throws Exception;
+
+    /**
+     * 分页查询员工列表
+     * @param example 条件实体
+     * @param pageUtil 分页条件
+     * @return
+     * @throws Exception
+     */
+    PageUtil<Worker> selectByExample(WorkerCriteria example, PageUtil pageUtil) throws Exception;
 
 }
